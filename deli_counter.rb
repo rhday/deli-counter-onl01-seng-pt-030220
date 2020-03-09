b58fbe808e7b495ef43e27a4c14a2b87db78e840
Ruari Day <ruariday@gmail.com>
2020-03-09 16:30:40 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,50 @@
 # Write your code here.
+def line(customers)
+  if customers.length >= 1 
+    new_array = []
+    index = 1
+    customers.each do |name|
+      new_array << "#{index}. #{name}"
+      index += 1
+    end
+    puts "The line is currently: #{new_array.join(" ")}"
+  else
+    puts "The line is currently empty."
+  end
+end
+
+def take_a_number()
+end
+
+
+
+
+#below are previous attempts which failed
+=begin 
+def line(customers)
+  current_line = "The line is currently:"
+  customers.each.with_index(1) do |name, number|
+    current_line << " #{number}. #{name}"
+  end 
+  puts current_line
+end
+=end
+=begin def line(katz_deli)
+  deli_line = []
+  if katz_deli.length > i do
+    katz_deli.each.with_index(1) do |name, position|
+      position = "The line is currently: #{position}. #{name}."
+    end
+  else puts "The line is currently empty."
+  end
+end
+=end
+
+=begin def assign_rooms(attendees)
+  rooms =[]
+  attendees.each.with_index(1) do |name, room_number|
+     rooms << room_number = "Hello, #{name}! You'll be assigned to room #{room_number}!"
+  end
+  rooms
+end 
+=end
