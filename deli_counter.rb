9f66b7b3666dbfe79c972b50b07726072d3b665f
Ruari Day <ruariday@gmail.com>
2020-03-09 16:41:28 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -17,7 +17,9 @@ def take_a_number(katz_deli, new_customer)
   katz_deli << new_customer
   puts "Welcome, #{new_customer}. You are number #{katz_deli.length} in line."
 end
-
+def now_serving(katz_deli)
+  
+end
 
 
 
