0ce0e7b5a7b8dbb82a0b9c03dd42003bbb9cf1d0
Ruari Day <ruariday@gmail.com>
2020-03-09 16:43:06 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -18,7 +18,9 @@ def take_a_number(katz_deli, new_customer)
   puts "Welcome, #{new_customer}. You are number #{katz_deli.length} in line."
 end
 def now_serving(katz_deli)
-  if katz_deli.legnth ==
+  if katz_deli.legnth == 0 
+    puts "There is no "
+  end
 end
 
 
